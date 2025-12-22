6ed9f260a46205beab25dcc552d40f3b6f312a3e
Dennis <d.schaible@pivot-recruiting.de>
2025-12-22 20:30:02 +0700

    updated: connnected sentry to bridge app

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import * as Sentry from "@sentry/electron";
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-)
+Sentry.init({
+  dsn: "https://a534ee90c276b99d94aec4c22e6fc8c3@o4510578425135104.ingest.de.sentry.io/4510578677645392",
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
