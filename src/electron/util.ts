e2c8306d28b7a9835e6f459ee57c870d2b88f03a
Dennis <d.schaible@pivot-recruiting.de>
2025-12-24 14:02:41 +0700

    .

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -9,27 +9,35 @@ const PORT = process.env.PORT || "5173"; // Default to Vite's default port
 
 // Checks if you are in development mode
 export function isDev(): boolean {
-    return process.env.NODE_ENV == "development";
+  return process.env.NODE_ENV == "development";
 }
 
 // Making IPC Typesafe
 export function ipcMainHandle<Key extends keyof EventPayloadMapping>(
-    key: Key, 
-    handler: (event: Electron.IpcMainInvokeEvent, ...args: any[]) => EventPayloadMapping[Key] | Promise<EventPayloadMapping[Key]>
+  key: Key,
+  handler: (
+    event: Electron.IpcMainInvokeEvent,
+    ...args: any[]
+  ) => EventPayloadMapping[Key] | Promise<EventPayloadMapping[Key]>
 ) {
-    ipcMain.handle(key as string, (event, ...args) => {
-        if (event.senderFrame) validateEventFrame(event.senderFrame);
+  ipcMain.handle(key as string, (event, ...args) => {
+    if (event.senderFrame) validateEventFrame(event.senderFrame);
 
-        return handler(event, ...args);
-    });
+    return handler(event, ...args);
+  });
 }
 
-export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(key: Key, webContents: WebContents, payload: EventPayloadMapping[Key]) {
-    webContents.send(key as string, payload);
+export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
+  key: Key,
+  webContents: WebContents,
+  payload: EventPayloadMapping[Key]
+) {
+  webContents.send(key as string, payload);
 }
 
 export function validateEventFrame(frame: WebFrameMain) {
-    if (isDev() && new URL(frame.url).host === `localhost:${PORT}`) return;
+  if (isDev() && new URL(frame.url).host === `localhost:${PORT}`) return;
 
-    if (frame.url !== pathToFileURL(getUIPath()).toString()) throw new Error("Malicious event");
+  if (frame.url !== pathToFileURL(getUIPath()).toString())
+    throw new Error("Malicious event");
 }
